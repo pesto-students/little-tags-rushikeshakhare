b67bdab131452b1e16cb68737a031cbcab90aec2
= <=>
2020-12-17 23:34:01 +0530

    Add throttle utility

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,1 +1,11 @@
-export const t = 1;
+export function throttle(fn: any, timeInMs: number) {
+  let timeout: any = null;
+  return (...args: any[]) => {
+    if (timeout) return;
+    timeout = setTimeout(function (this: any) {
+      const context = this;
+      fn.apply(context, args);
+      timeout = null;
+    }, timeInMs);
+  };
+}
