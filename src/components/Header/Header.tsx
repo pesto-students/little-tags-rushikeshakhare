72346203a800ca29d3ce7ba50e1b3f02ea4eb7bb
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2020-12-18 00:18:14 +0530

    Add login & signup button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,9 +15,10 @@ export enum HeaderType {
 interface IHeaderProps {
   type: HeaderType;
   onMenuButtonClick: () => void;
+  isAuthenticated: boolean;
 }
 
-export const Header = ({ onMenuButtonClick, type }: IHeaderProps) => {
+export const Header = ({ onMenuButtonClick, type, isAuthenticated }: IHeaderProps) => {
   return (
     <div
       className={`nav-header fixed ${
@@ -43,13 +44,24 @@ export const Header = ({ onMenuButtonClick, type }: IHeaderProps) => {
           alt="Cart Icon"
         />
       </div>
-      <div className="user-data right">
-        <img
-          src={type === HeaderType.BLACK ? iconUserAvatar : iconUserAvatarWhite}
-          alt="User Avatar Icon"
-        />
-        <div className="user-text right">Ashim Raj Konwar</div>
-      </div>
+      
+      {
+        isAuthenticated === true &&
+        <div className="user-data right">
+            <img
+            src={type === HeaderType.BLACK ? iconUserAvatar : iconUserAvatarWhite}
+            alt="User Avatar Icon"
+            />
+            <div className="user-text right">Ashim Raj Konwar</div>
+        </div>
+      }
+
+      {
+          isAuthenticated === false &&
+          <div className="user-auth-button right">
+            <a href="#" onClick={(e) => { e.preventDefault(); }} className="user-text right">Login / Signup</a>
+          </div>
+      }
 
       <div className="search-bar-container item-center pointer text-left">
         <div className="search-bar">Search bar (remove border once done)</div>
--- src/testbed/fixtures/Header.ts
+++ src/testbed/fixtures/Header.ts
@@ -2,6 +2,7 @@ import { HeaderType } from "../../components/Header/Header";
 
 const fixture = {
   type: HeaderType.WHITE,
+  isAuthenticated: false,
 };
 
 export default fixture;
