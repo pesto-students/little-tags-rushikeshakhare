801619bd06b15b912c771e4cdca588269b2ca01d
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2020-12-17 23:38:34 +0530

    Add testbed  cased for product card

--- src/testbed/fixtures/index.ts
+++ src/testbed/fixtures/index.ts
@@ -3,6 +3,7 @@ import Menu from "./Menu";
 import Header from "./Header";
 import Footer from "./Footer";
 import Hero from "./Hero";
+import ProductCard from "./ProductCard";
 
 const allFixtues = {
   Login,
@@ -10,6 +11,7 @@ const allFixtues = {
   Header,
   Footer,
   Hero,
+  ProductCard
 };
 
 export default allFixtues;
