beeaaed938924edf66e9803616056d94be66fa53
= <=>
2020-12-23 21:13:48 +0530

    Wrap Home Screen with container hook

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import { Hero } from "../../components";
 import { Categories } from "./Categories";
+import { withContainer } from "../../hocs/withContainer";
 import "./home.scss";
 
 interface IHomeProps {
   history: any;
 }
 
-export const Home = ({ history }: IHomeProps) => {
+export const Home = withContainer(({ history }: IHomeProps) => {
   return (
     <>
       <Hero />
       <Categories navigateToCategory={history.push} />
     </>
   );
-};
+});
